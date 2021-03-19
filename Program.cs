06e0dc610cf63c5bc604e14c9bc93e71daac1eee
sumdu <oleshkoa@gmail.com>
2021-03-18 22:41:53 -0400

    Test

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace SingleInstanceApp
 {
@@ -7,6 +7,7 @@ namespace SingleInstanceApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
         }
     }
 }
